18c95f9613d7aa3f9c35f82c04ad95fe14b990e0
Naren <nmanikandan219@gmail.com>
2022-03-21 14:59:14 -0400

    Minor updates

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -135,7 +135,11 @@ function addText (table, idx, name, data) {
   const cell2 = row.insertCell(1)
   cell1.innerHTML = name + '&nbsp;'
   cell2.classList.add('field')
-  const inp = document.createElement('input')
+  if (data.code == "co") {
+    const inp = document.createElement('textarea')
+  } else {
+    const inp = document.createElement('input')
+  }
   inp.setAttribute('id', 'input_' + data.code)
   inp.setAttribute('type', 'text')
   inp.setAttribute('name', data.code)
